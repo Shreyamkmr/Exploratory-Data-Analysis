a412c932292f6a552cdc6871732e17d06db9d41d
Shreyamkmr <shreyamkmr@gmail.com>
2020-05-20 19:12:24 +0530

    commit 2nd

--- Plot2.R
+++ Plot2.R
@@ -1,18 +1,26 @@
-# Reading, naming and subsetting power consumption data
-power <- read.table("household_power_consumption.txt",skip=1,sep=";")
-names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+library("data.table")
 
-# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
-subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
-subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
-subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
-subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+setwd("~/Desktop/data")
 
+#Reads in data from file then subsets data for specified dates
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+)
 
-# calling the basic plot function
-plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+# Prevents Scientific Notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
-# annotating graph
-title(main="Global Active Power Vs Time")
-dev.copy2pdf(file="Plot2.pdf")
+# Making a POSIXct date capable of being filtered and graphed by time of day
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot2.png", width=480, height=480)
+
+## Plot 2
+plot(x = powerDT[, dateTime]
+     , y = powerDT[, Global_active_power]
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
